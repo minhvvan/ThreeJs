9dbfba20a8bcc5eaef64f95d73cb7f2ab6f6b954
GR8 <eminhwan12@gmail.com>
2021-09-17 18:33:19 +0900

    create portal

---
+++ portal/src/script.js
@@ -0,0 +1,225 @@
+import './style.css'
+import * as dat from 'dat.gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragmentShader from './shaders/fireflies/fragment.glsl'
+import portalVertexShader from './shaders/Portal/vertex.glsl'
+import portalFragmentShader from './shaders/Portal/fragment.glsl'
+
+//NOTE Spector
+// const SPECTOR = require('spectorjs')
+// const spector = new SPECTOR.Spector()
+// spector.displayUI()
+
+//NOTE Debug
+const debudObject = {}
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI({
+    width: 400
+})
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+
+//NOTE Materials
+debudObject.portalColorStart = '#091150'
+debudObject.portalColorEnd = '#5e7ce3'
+
+//Pole light
+const poleLightMaterial = new THREE.MeshBasicMaterial({color: 0xffffe5})
+const portalLightMaterial = new THREE.ShaderMaterial({
+    uniforms:{
+        uTime: { value: 0 },
+        uColorStart: { value: new THREE.Color(debudObject.portalColorStart) },
+        uColorEnd: { value: new THREE.Color(debudObject.portalColorEnd) },
+    },
+    vertexShader: portalVertexShader,
+    fragmentShader: portalFragmentShader
+})
+
+gui.addColor(debudObject,'portalColorStart')
+    .onChange(() => {
+        portalLightMaterial.uniforms.uColorStart.value.set(debudObject.portalColorStart)
+    })
+    
+
+gui.addColor(debudObject,'portalColorEnd')
+.onChange(() => {
+    portalLightMaterial.uniforms.uColorEnd.value.set(debudObject.portalColorEnd)
+})
+
+
+//Texture
+const bakedTexture = textureLoader.load('baked.jpg')
+bakedTexture.flipY = false
+bakedTexture.encoding = THREE.sRGBEncoding
+
+const bakedMaterial = new THREE.MeshBasicMaterial({map: bakedTexture})
+
+
+//Model
+gltfLoader.load(
+    'portal.glb',
+    (gltf) => {
+        const bakedMesh = gltf.scene.children.find((child) => child.name === 'baked')
+        const poleLightAMesh = gltf.scene.children.find((child) => child.name === 'poleLightA')
+        const poleLightBMesh = gltf.scene.children.find((child) => child.name === 'poleLightB')
+        const portalLightMesh = gltf.scene.children.find((child) => child.name === 'portalLight')
+
+        bakedMesh.material = bakedMaterial
+        poleLightAMesh.material = poleLightMaterial
+        poleLightBMesh.material = poleLightMaterial
+        portalLightMesh.material = portalLightMaterial
+
+        scene.add(gltf.scene)
+    }
+)
+
+//NOTE Fireflies
+//geo
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 30
+const positionArray = new Float32Array(firefliesCount * 3)
+const scaleArray = new Float32Array(firefliesCount)
+
+for (let i = 0; i < firefliesCount; i++) {
+    positionArray[i * 3 + 0] = (Math.random() - 0.5) *4
+    positionArray[i * 3 + 1] = Math.random() * 1.5
+    positionArray[i * 3 + 2] = (Math.random() -0.5) *4
+    
+    scaleArray[i] = Math.random()
+}
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+firefliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1))
+
+//Meterial
+const firefliesMaterial = new THREE.ShaderMaterial({
+    uniforms:{
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 100 },
+        uTime: { value: 0 }
+    },
+    vertexShader: firefliesVertexShader,
+    fragmentShader: firefliesFragmentShader,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    transparent: true
+})
+gui.add(firefliesMaterial.uniforms.uSize, 'value').min(0).max(500).step(1).name("firefliesSize")
+
+//Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial) 
+scene.add(fireflies)
+
+
+//NOTE Portal
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    //Update fireflies
+    firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 4
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.outputEncoding = THREE.sRGBEncoding
+
+debudObject.clearColor = '#2a2a2d'
+renderer.setClearColor(debudObject.clearColor)
+gui.addColor(debudObject,'clearColor').onChange( () => {
+    renderer.setClearColor(debudObject.clearColor)
+})
+
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    //Update shader
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
+    portalLightMaterial.uniforms.uTime.value = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
